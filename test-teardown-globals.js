eaae928f8115059520aa59c3f60a4615ba8e2d65
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2021-10-09 18:16:16 +0100

    add comment to explain extra test teardown file

--- test-teardown-globals.js
+++ test-teardown-globals.js
@@ -1,3 +1,5 @@
+// This is to fix some memory leak issues I ran into when testing API routes
+// To resolve this I followed the solution described here: https://github.com/visionmedia/supertest/issues/520#issuecomment-768148875
 module.exports = () => {
   process.exit(0);
 };
